e4e4d48c0b10df367531e9ad934b3cf4b8e5aba2
Márton Elek <elek@apache.org>
2018-12-17 12:56:39 +0100

    print help if no subcommand is selected

--- cmd/root.go
+++ cmd/root.go
@@ -8,24 +8,18 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
         Use:   "flekszible",
-        Short: "A brief description of your application",
-        Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-
-        Run: func(cmd *cobra.Command, args []string) {
-        },
+        Short: "A highly flexible kubernetes resource manager",
+        Long:  ``,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+
+        rootCmd.RunE = func(cmd *cobra.Command, args []string) error {
+                return rootCmd.Help()
+        }
+
         if err := rootCmd.Execute(); err != nil {
                 fmt.Println(err)
                 os.Exit(1)
@@ -35,7 +29,4 @@ func Execute() {
 func init() {
         cobra.OnInitialize()
 
-        // Cobra also supports local flags, which will only run
-        // when this action is called directly.
-        // rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

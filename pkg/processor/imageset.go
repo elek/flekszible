f6e6aea68c105c7a8cb28ff95d7ef2d41fc3a732
Márton Elek <elek@apache.org>
2019-02-09 09:10:15 +0100

    trigger support for image set

--- pkg/processor/imageset.go
+++ pkg/processor/imageset.go
@@ -4,11 +4,14 @@ import "github.com/elek/flekszible/pkg/data"
 
 type Image struct {
 	DefaultProcessor
-	Image string
+	Image   string
+	Trigger Trigger
 }
 
 func (imageSet *Image) BeforeResource(resource *data.Resource) {
-	resource.Content.Accept(&data.Set{Path: data.NewPath("spec", "template", "spec", "(initC|c)ontainers", ".*", "image"), NewValue: imageSet.Image})
+	if imageSet.Trigger.active(resource) {
+		resource.Content.Accept(&data.Set{Path: data.NewPath("spec", "template", "spec", "(initC|c)ontainers", ".*", "image"), NewValue: imageSet.Image})
+	}
 }
 func init() {
 	prototype := Image{}

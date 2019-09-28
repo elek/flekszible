100498fc71d745f56ed300840f4d7c0172da32f0
Márton Elek <elek@apache.org>
2019-09-28 22:26:35 +0200

    fix unit test

--- pkg/examples/source_test.go
+++ pkg/examples/source_test.go
@@ -1,10 +1,13 @@
 package examples
 
 import (
+	"github.com/elek/flekszible/api/data"
 	"github.com/elek/flekszible/api/processor"
+	"github.com/elek/flekszible/pkg"
 	"testing"
 )
 
 func TestSource(t *testing.T) {
+	data.DownloaderPlugin = pkg.GoGetterDownloader{}
 	processor.TestFromDir(t, "source")
 }

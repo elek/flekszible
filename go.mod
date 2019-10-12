6aa17cc9853853290d23999013348637e5d99c03
Márton Elek <elek@apache.org>
2019-10-12 10:35:18 +0200

    replace cobra with urfave/cli

--- go.mod
+++ go.mod
@@ -4,17 +4,14 @@ require (
 	github.com/brettski/go-termtables v0.0.0-20190907034855-12ddd59af020
 	github.com/elek/flekszible/api v0.0.0-20190928090338-d1d34ef1bc1f
 	github.com/hashicorp/go-getter v1.1.0
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/kisielk/errcheck v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.2.0
-	github.com/spf13/cobra v0.0.3
-	github.com/spf13/pflag v1.0.2 // indirect
+	github.com/urfave/cli v1.22.1
 	golang.org/x/crypto v0.0.0-20190927123631-a832865fa7ad // indirect
 	golang.org/x/net v0.0.0-20190926025831-c00fd9afed17 // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20190927073244-c990c680b611 // indirect
 	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20190929041059-e7abfedfabcf // indirect
 )
 
 replace github.com/elek/flekszible/api => ./api
+
+go 1.13
--- main.go
+++ main.go
@@ -1,9 +1,210 @@
 package main
 
-import "github.com/elek/flekszible/cmd"
+// import "github.com/elek/flekszible/cmd"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/elek/flekszible/api/processor"
+	"github.com/elek/flekszible/pkg"
+	"github.com/urfave/cli"
+)
+
+var version string
+var commit string
+var date string
 
 func main() {
 
-	cmd.Execute()
+	var inputDir, outputDir, imageOverride, namespaceOverride string
+	var minikube bool
+	app := cli.NewApp()
+	app.Name = "Flekszible"
+	app.Usage = "Generate kubernetes sources files"
+	app.Description = "Kubernetes resource file generator"
+	app.Version = fmt.Sprintf("%s (%s, %s)", version, commit, date)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "source, s",
+			Value:       "",
+			Usage:       "Source directory to read the Flekszible definition (default: current dir)",
+			Destination: &inputDir,
+		},
+		cli.StringFlag{
+			Name:        "destination, d",
+			Value:       "",
+			Usage:       "Destination directory to generate the k8s resource (default: current dir)",
+			Destination: &outputDir,
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:    "generate",
+			Aliases: []string{"g"},
+			Usage:   "Genrate kubernetes resources files",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "namespace, n",
+					Value: "",
+					Usage: "kubernetes namespace override. With empty value (\"\") the current namespace will " +
+						"be used. With exact value the namespace will be forced (set even if the namespace is not added to the to the k8s resources",
+					Destination: &namespaceOverride,
+				},
+				cli.StringFlag{
+					Name:        "image, i",
+					Usage:       "docker image name override",
+					Destination: &imageOverride,
+				},
+				cli.BoolFlag{
+					Name:        "minikube",
+					Usage:       "Enable minikube specific defaults (eg. daemonset to statefulset conversion)",
+					Destination: &minikube,
+				},
+			},
+			Action: func(c *cli.Context) error {
+				context := processor.CreateRenderContext("k8s", findInputDir(&inputDir), findOutputDir(&outputDir))
+				context.ImageOverride = imageOverride
+				context.Namespace = namespaceOverride
+				pkg.Run(context, minikube)
+				return nil
+			},
+		},
+		{
+			Name:  "list",
+			Usage: "List managed kubernetes resources files.",
+			Action: func(c *cli.Context) error {
+				context := processor.CreateRenderContext("k8s", findInputDir(&inputDir), findOutputDir(&outputDir))
+				pkg.ListResources(context)
+				return nil
+			},
+		},
+		appCommands(&inputDir, &outputDir),
+		sourceCommands(&inputDir, &outputDir),
+		transformationCommands(&inputDir, &outputDir),
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
+func appCommands(inputDir *string, outputDir *string) cli.Command {
+	return cli.Command{
+		Name:  "app",
+		Usage: "Manage importable dirs/applications.",
+		Subcommands: []cli.Command{
+			{
+				Name:  "list",
+				Usage: "List all the referenced/imported flekszible directory.",
+				Action: func(c *cli.Context) error {
+					context := processor.CreateRenderContext("k8s", findInputDir(inputDir), findOutputDir(outputDir))
+					pkg.ListApp(context)
+					return nil
+				},
+			},
+			{
+				Name:  "search",
+				Usage: "Search for available importable apps/dirs from the active sources",
+				Action: func(c *cli.Context) error {
+					context := processor.CreateRenderContext("k8s", findInputDir(inputDir), findOutputDir(outputDir))
+					pkg.SearchComponent(context)
+					return nil
+				},
+			},
+			{
+				Name:  "add",
+				Usage: "Add (import) new app to the flekszible.yaml.",
+				Action: func(c *cli.Context) error {
+					context := processor.CreateRenderContext("k8s", findInputDir(inputDir), findOutputDir(outputDir))
+					pkg.AddApp(context, *inputDir, c.Args().First())
+					return nil
+				},
+			},
+		},
+	}
+}
+
+func sourceCommands(inputDir, outputDir *string) cli.Command {
+	return cli.Command{
+		Name:  "source",
+		Usage: "Manage importable dirs/applications.",
+		Subcommands: []cli.Command{
+			{
+				Name:  "list",
+				Usage: "List registered sources (directories / repositories where other directories are imported from)",
+				Action: func(c *cli.Context) error {
+					context := processor.CreateRenderContext("k8s", findInputDir(inputDir), findOutputDir(outputDir))
+					pkg.ListApp(context)
+					return nil
+				},
+			},
+			{
+				Name:  "search",
+				Usage: "Search for the available source repositories (github repositories with flekszible:topic).",
+				Action: func(c *cli.Context) error {
+					pkg.SearchSource()
+					return nil
+				},
+			},
+			{
+				Name:  "add",
+				Usage: "Register source to your Flekszible descriptor file",
+				Action: func(c *cli.Context) error {
+					context := processor.CreateRenderContext("k8s", findInputDir(inputDir), findOutputDir(outputDir))
+					pkg.AddSource(context, findInputDir(inputDir), c.Args().First())
+					return nil
+				},
+			},
+		},
+	}
+}
+
+func transformationCommands(inputDir, outputDir *string) cli.Command {
+	return cli.Command{
+		Name:  "type",
+		Usage: "Show available transformation types/definitions",
+		Subcommands: []cli.Command{
+			{
+				Name:  "list",
+				Usage: "List available transformation types.",
+				Action: func(c *cli.Context) error {
+					context := processor.CreateRenderContext("k8s", findInputDir(inputDir), findOutputDir(outputDir))
+					pkg.ListProcessor(context)
+					return nil
+				},
+			},
+			{
+				Name:  "show",
+				Usage: "Show details information / help about a specific transformation type",
+				Action: func(c *cli.Context) error {
+					context := processor.CreateRenderContext("k8s", findInputDir(inputDir), findOutputDir(outputDir))
+					pkg.ShowProcessor(context, c.Args().First())
+					return nil
+				},
+			},
+		},
+	}
+}
+
+func findInputDir(argument *string) string {
+	if *argument != "" {
+		return *argument
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return pwd
+}
+
+func findOutputDir(argument *string) string {
+	if *argument != "" {
+		return *argument
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return pwd
 }

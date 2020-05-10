f7ccffd3eb7cabddd1274619f6306fb8338d64f7
Elek Márton <elek@apache.org>
2020-05-10 13:18:18 +0200

    disable outputgenerator overwrite of executable

--- api/data/outputgenerator.go
+++ api/data/outputgenerator.go
@@ -1,7 +1,9 @@
 package data
 
 import (
+	"bytes"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
 	"path"
@@ -31,6 +33,23 @@ func (*OutputGenerator) Generate(sourceDir string, destinationDir string) ([]*Re
 			stat, err := os.Stat(sourceFile)
 
 			destinationFile := path.Join(destinationDir, filename)
+
+			if stat.Mode().Perm()&0444 > 0 {
+				//executable files are written only if doesn't exist
+				if _, err = os.Stat(destinationFile); !os.IsNotExist(err) {
+					destinationContent, err := ioutil.ReadFile(destinationFile)
+					if err != nil {
+						return resources, errors.Wrap(err, "Can't read the file from the destination: "+destinationFile)
+					}
+					if !bytes.Equal(content, destinationContent) {
+						logrus.Warn("Sourcefile is executable and destination file exists. Doesn't overwrite it for security reason. Please delete " + destinationFile)
+						return resources, nil
+					}
+				} else {
+					logrus.Warn("Copying executable file from " + sourceFile + " to " + destinationFile)
+				}
+
+			}
 			err = ioutil.WriteFile(destinationFile, content, stat.Mode())
 			if err != nil {
 				return resources, errors.Wrap(err, "Couldn't copy the file from "+sourceFile+" to "+destinationFile)

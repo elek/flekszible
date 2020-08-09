3b66f99e4c3af429b60945534c75508c68946734
Elek Márton <elek@apache.org>
2020-08-09 16:16:28 +0200

    [test] fix unit test

--- pkg/runner_test.go
+++ pkg/runner_test.go
@@ -11,7 +11,7 @@ func TestCreateTransformation(t *testing.T) {
 	proc, err := parseTransformation("image:image=asd")
 	assert.Nil(t, err)
 
-	image := proc[0].(*processor.Image)
+	image := proc.(*processor.Image)
 
 	assert.Equal(t, "asd", image.Image)
 }
@@ -21,22 +21,8 @@ func TestCreateTransformationImageWithColon(t *testing.T) {
 	proc, err := parseTransformation("image:image=localhost:5000/image/name:tag")
 	assert.Nil(t, err)
 
-	image := proc[0].(*processor.Image)
+	image := proc.(*processor.Image)
 
 	assert.Equal(t, "localhost:5000/image/name:tag", image.Image)
 }
 
-func TestCreateTransformationMultiple(t *testing.T) {
-
-	proc, err := parseTransformation("image:image=localhost:5000/image/name:tag;mount:path=/tmp,nhostPath=/tmp/test")
-	assert.Nil(t, err)
-
-	image := proc[0].(*processor.Image)
-
-	assert.Equal(t, "localhost:5000/image/name:tag", image.Image)
-
-	mount := proc[1].(*processor.Mount)
-
-	assert.Equal(t, "/tmp", mount.Path)
-	assert.Equal(t, "/tmp/test", mount.HostPath)
-}

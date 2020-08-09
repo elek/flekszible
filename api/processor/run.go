c780720d3aafff8626733b82491fed33959e940d
Elek Márton <elek@apache.org>
2020-08-09 09:05:19 +0200

    [trafo] introduce new run transformation

---
+++ api/processor/run.go
@@ -0,0 +1,49 @@
+package processor
+
+import (
+	"github.com/elek/flekszible/api/data"
+	"github.com/elek/flekszible/api/yaml"
+	"strings"
+)
+
+type Run struct {
+	DefaultProcessor
+	Args    string
+	Trigger Trigger
+}
+
+func (run *Run) BeforeResource(resource *data.Resource) error {
+	if run.Trigger.active(resource) {
+
+		args := strings.Split(run.Args, " ")
+		smartGet := data.SmartGetAll{Path: data.NewPath("spec", "template", "spec", "containers", ".*")}
+		resource.Content.Accept(&smartGet)
+		for _, result := range smartGet.Result {
+			container := result.Value.(*data.MapNode)
+			newArgs := container.CreateList("args")
+			for _, arg := range args {
+				newArgs.AddValue(arg)
+			}
+		}
+	}
+	return nil
+}
+func init() {
+	ProcessorTypeRegistry.Add(ProcessorDefinition{
+		Metadata: ProcessorMetadata{
+			Name:        "Run",
+			Description: "Replace args wi",
+			Doc:         "Space separated string will be used as array",
+			Parameter: []ProcessorParameter{
+				TriggerParameter,
+				{
+					Name:        "args",
+					Description: "Space separated arguments to use as args",
+				},
+			},
+		},
+		Factory: func(config *yaml.MapSlice) (Processor, error) {
+			return configureProcessorFromYamlFragment(&Run{}, config)
+		},
+	})
+}
---
+++ api/processor/run_test.go
@@ -0,0 +1,9 @@
+package processor
+
+import (
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	TestFromDir(t, "run")
+}

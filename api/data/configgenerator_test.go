3a506c283ff1158af86347ac965ff6cb7a96c55c
Elek Márton <elek@apache.org>
2020-08-17 23:16:22 +0200

    [test] remove test part which depends on fs file order

--- api/data/configgenerator_test.go
+++ api/data/configgenerator_test.go
@@ -8,18 +8,13 @@ func TestReadConfigMaps(t *testing.T) {
 	resources, err := generator.Generate("../../testdata/configmaps", "/tmp")
 	assert.Nil(t, err)
 	assert.Equal(t, 2, len(resources))
-
-	nameConfig, err := ReadManifestFile("../../testdata/configmaps/expected/config.yaml")
-	assert.Nil(t, err)
-
+	
 	otherConfig, err := ReadManifestFile("../../testdata/configmaps/expected/config2.yaml")
 	assert.Nil(t, err)
 
 	if resources[0].Name() == "name" {
-		assert.Equal(t, nameConfig, resources[0].Content)
 		assert.Equal(t, otherConfig, resources[1].Content)
 	} else {
-		assert.Equal(t, nameConfig, resources[1].Content)
 		assert.Equal(t, otherConfig, resources[0].Content)
 	}
 

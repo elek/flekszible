0aba5e4a654ea3666b8ca0c77c12b84850844113
Elek Márton <elek@apache.org>
2020-08-17 23:07:48 +0200

    [test] make TestReadConfigMaps independent from the order of resources

--- api/data/configgenerator_test.go
+++ api/data/configgenerator_test.go
@@ -9,14 +9,18 @@ func TestReadConfigMaps(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, 2, len(resources))
 
-	expected, err := ReadManifestFile("../../testdata/configmaps/expected/config.yaml")
+	nameConfig, err := ReadManifestFile("../../testdata/configmaps/expected/config.yaml")
 	assert.Nil(t, err)
 
-	assert.Equal(t, expected, resources[0].Content)
-
-	expected, err = ReadManifestFile("../../testdata/configmaps/expected/config2.yaml")
+	otherConfig, err := ReadManifestFile("../../testdata/configmaps/expected/config2.yaml")
 	assert.Nil(t, err)
 
-	assert.Equal(t, expected, resources[1].Content)
+	if resources[0].Name() == "name" {
+		assert.Equal(t, nameConfig, resources[0].Content)
+		assert.Equal(t, otherConfig, resources[1].Content)
+	} else {
+		assert.Equal(t, nameConfig, resources[1].Content)
+		assert.Equal(t, otherConfig, resources[0].Content)
+	}
 
 }

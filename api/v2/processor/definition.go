7539df997253cc6fd614f190810866d71620dfef
Elek Márton <elek@apache.org>
2021-03-04 16:24:55 +0100

    optional extension points are logged only on debug level

--- api/v2/processor/definition.go
+++ api/v2/processor/definition.go
@@ -46,7 +46,7 @@ func (registry *ProcessorTypes) ReadProcessorDefinition(data []byte) ([]Processo
 			if err != nil {
 				return processors, errors.Wrap(err, "Transformation can't be instantiated: "+typeName.(string)+" "+err.Error())
 			} else if proc == nil {
-				logrus.Info("Optional transformation depends on an unknown transformation type: " + typeName.(string) + " Additional import may be required to use optional features.")
+				logrus.Debug("Optional transformation depends on an unknown transformation type: " + typeName.(string) + " Additional import may be required to use optional features.")
 			} else {
 				if scope, found := processorConfig.Get("scope"); found {
 					proc.SetScope(scope.(string))
